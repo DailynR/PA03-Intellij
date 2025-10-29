c82e906df002a9b5f42451f2e8af987374146228
DailynR <113866985+DailynR@users.noreply.github.com>
2025-10-28 22:09:58 -0400

    Hi, here is PA03 (Part 1)

---
+++ src/main/java/com/example/pa03/store/Store.java
@@ -0,0 +1,7 @@
+package com.example.pa03.store;
+
+public class Store {
+}
+private final Map<String, ResourceDTO> byId = new HashMap<>();
+private final List<ResourceDTO> all = new ArrayList<>();
+
---
+++ src/test/java/com/example/pa03/Pa03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pa03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
